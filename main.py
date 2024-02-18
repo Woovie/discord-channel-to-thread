0c438ed82278b1f81373ba7e0a8defc3e52c751a
Jordan Banasik <woovie@woovie.net>
2024-02-17 16:09:22 -0800

    stuff

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import configparser
 import discord
+import pathlib
+import os
 
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+settings = {}
+
+if pathlib.Path('config.ini').exists() == True:
+  settings['type'] = 'config.ini'
+  config = configparser.ConfigParser()
+  config.read('config.ini')
+  settings['id'] = config['discord']['id']
+  settings['token'] = config['discord']['token']
+  settings['source_guild'] = config['discord']['source_guild']
+  settings['source_channel'] = config['discord']['source_channel']
+  settings['destination_guild'] = config['discord']['destination_guild']
+  settings['destination_thread'] = config['discord']['destination_thread']
+# Otherise, use environment variables
+elif 'DISCORD_ID' in os.environ:
+  settings['type'] = 'environment variables'
+  settings['id'] = os.environ['DISCORD_ID']
+  settings['token'] = os.environ['DISCORD_TOKEN']
+  settings['source_guild'] = int(os.environ['DISCORD_SOURCE_GUILD'])
+  settings['source_channel'] = int(os.environ['DISCORD_SOURCE_CHANNEL'])
+  settings['destination_guild'] = int(os.environ['DISCORD_DESTINATION_GUILD'])
+  settings['destination_thread'] = int(os.environ['DISCORD_DESTINATION_THREAD'])
+else:
+  print("config.ini missing and environment variables not set. Exiting.")
+  exit()
 
 # Set up intents for reading message contents
 intents = discord.Intents.default()
@@ -18,12 +42,13 @@ async def on_ready():
 @client.event
 async def on_message(message: discord.Message):
   # Check if the message was in config[discord][source_guild] and config[discord][source_channel]
-  if message.guild.id == int(config['discord']['source_guild']) and message.channel.id == int(config['discord']['source_channel']):
+  if message.guild.id == settings['source_guild'] and message.channel.id == settings['source_channel']:
     # Send the message to the destination guild, channel, thread
-    guild = client.get_guild(int(config['discord']['destination_guild']))
-    thread = await guild.fetch_channel(int(config['discord']['destination_thread']))
+    guild = client.get_guild(settings['destination_guild'])
+    thread = await guild.fetch_channel(settings['destination_thread'])
     print(message.content)
     await thread.send(message.content)
 
 if __name__ == '__main__':
-  client.run(config['discord']['token'])
+  print(f"Loaded settings via {settings['type']}")
+  client.run(settings['token'])
